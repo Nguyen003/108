1b055de3c23609f3d2729e1abb9dc9bf7187cc58
quy <nxuan0212@gmail.com>
2024-07-12 16:09:37 +0700

    setup source, add login, home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import '~/assets/fonts/font-awesome-6.5.2/css/all.min.css';
+
+import Login from '~/pages/Login'  
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
     </div>
   );
 }
