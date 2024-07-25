fef288983c8d35c227a99b43a4d4f87f8fefe537
quy <nxuan0212@gmail.com>
2024-07-25 15:14:56 +0700

    fix ui - làm lại login - thêm camera - thêm chekbox (map) - popup vị trí  (map)

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import cam1 from './cam1.jpg';
+import cam2 from './cam2.jpg';
+import cam3 from './cam3.jpg';
+import cam4 from './cam4.jpg';
+import locationIcon from './location-icon.png';
+import logo from './logo.png';
+import logoTeca from './logotecapro.jpg';
+import sodo from './so_do.jpg';
+import bgLogin from './bg_login.svg';
+
+export {cam1, cam2, cam3, cam4, locationIcon, logo, logoTeca, sodo, bgLogin}
